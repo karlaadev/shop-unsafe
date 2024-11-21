bdd9447ec6df83202a9ebcc10314516c6656fa86
Karlaadev <karlaadev11@gmail.com>
2024-11-21 00:39:28 -0600

    creacion estructura del proyecto

---
+++ nsf/src/main/java/com/unsafeshop/nsf/NsfApplication.java
@@ -0,0 +1,14 @@
+package com.unsafeshop.nsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class NsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NsfApplication.class, args);
+	}
+
+}
---
+++ nsf/src/test/java/com/unsafeshop/nsf/NsfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unsafeshop.nsf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NsfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
