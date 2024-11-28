7c0168e6421c0b4ab2451949e6faeda246310710
Karlaadev <karlaadev11@gmail.com>
2024-11-27 22:56:12 -0600

    crear las clases model, mapeo clases a tablas con JPA, integrado diseno y vistas

--- nsf/src/main/java/com/unsafeshop/nsf/model/Producto.java
+++ nsf/src/main/java/com/unsafeshop/nsf/model/Producto.java
@@ -1,6 +1,13 @@
 package com.unsafeshop.nsf.model;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Productos")
 public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nombre;
     private String descripcion;
@@ -8,16 +15,20 @@ public class Producto {
     private double precio;
     private int cantidad;
 
+    @ManyToOne
+    private Usuario usuario;
+
     public Producto() {
     }
 
-    public Producto(Integer id, String nombre, String descripcion, String imagen, double precio, int cantidad) {
+    public Producto(Integer id, String nombre, String descripcion, String imagen, double precio, int cantidad, Usuario usuario) {
         this.id = id;
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.imagen = imagen;
         this.precio = precio;
         this.cantidad = cantidad;
+        this.usuario = usuario;
     }
 
     public Integer getId() {
@@ -67,6 +78,16 @@ public class Producto {
     public void setCantidad(int cantidad) {
         this.cantidad = cantidad;
     }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+
 
     @Override
     public String toString() {
